c419db9b535c956e6dece2e1a0123fde27c3693a
Wojciech <wojjmaj22@gmail.com>
2022-05-19 18:02:45 +0200

    Zmiany

--- src/main/java/com/example/grafy/HelloController.java
+++ src/main/java/com/example/grafy/HelloController.java
@@ -4,7 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.concurrent.ExecutionException;
+
 public class HelloController {
+
+	// Ogarnąć wyjątki itd., spawdzić czy to jest OK?
+	// Dodać zapisywanie na drugi przycisk, ew. pobieranie pola też
 
 	@FXML
 	private Label wagaMinKrawedzi;
@@ -12,23 +17,56 @@ public class HelloController {
 	private Label wagaMaxKrawedzi;
 	@FXML
 	private TextField wagaKrawedzi;
+	@FXML
+	private TextField wymiarGrafu;
+	@FXML
+	private TextField ilePodzialow;
+	@FXML
+	private  TextField polePlikDoZapisu;
 
 	@FXML
 	protected void onGenerateButtonClick() {
-		String characters = wagaKrawedzi.getText();
-		try {
-			wagaKrawedzi.setStyle("-fx-background-color:white;");
-			int length = characters.length();
-			int myslnik = characters.indexOf('-');
-			String wagaMin = characters.substring(0, myslnik);
-			String wagaMax = characters.substring(myslnik + 1, length);
-			wagaMinKrawedzi.setText(wagaMin);
-			wagaMaxKrawedzi.setText(wagaMax);
-			generatorGrafu graf = new generatorGrafu(Integer.valueOf(wagaMin), Integer.valueOf(wagaMax));
-			graf.wypiszNaWyjscie();
-		} catch ( Exception e) {
+		String wagaZakres = wagaKrawedzi.getText();
+		String wymiary = wymiarGrafu.getText();
+		int wagaMin = 0;
+		int wagaMax = 0;
+		int wymiarX = 0;
+		int wymiarY = 0;
+		int iloscPodzialow = 0;
+		String plikDoZapisu = null;
+		try { // wczytywanie wymiarów
+			int indeksX = wymiary.indexOf('x');
+			wymiarX = Integer.valueOf(wymiary.substring(0, indeksX));
+			wymiarY = Integer.valueOf(wymiary.substring(indeksX + 1, wymiary.length()));
+		} catch ( Exception ee) {
+			wymiarGrafu.setStyle("-fx-background-color:red;");
+			wymiarGrafu.setPromptText("Brak podanych wymiarów");
+		}
+		try{ // wczytywanie wag krawędzi
+			int length = wagaZakres.length();
+			int myslnik = wagaZakres.indexOf('-');
+			wagaMin = Integer.valueOf(wagaZakres.substring(0, myslnik));
+			wagaMax = Integer.valueOf(wagaZakres.substring(myslnik + 1, length));
+			wagaMinKrawedzi.setText(String.valueOf(wagaMin));
+			wagaMaxKrawedzi.setText(String.valueOf(wagaMax));
+
+		} catch (Exception e) {
 			wagaKrawedzi.setStyle("-fx-background-color:red;");
 			wagaKrawedzi.setPromptText("Brak podanego zakresu");
 		}
+		try { // pobieranie ilości podzieleń grafu
+			iloscPodzialow = Integer.valueOf(ilePodzialow.getText());
+		} catch ( Exception a){
+			ilePodzialow.setStyle("-fx-background-color:red;");
+		}
+		try{ // pobieranie nazwy pliku do zapisania grafu
+			plikDoZapisu = polePlikDoZapisu.getText();
+		} catch	( Exception b){
+			polePlikDoZapisu.setStyle("-fx-background-color:red;");
+		}
+		generatorGrafu graf = new generatorGrafu(wagaMin, wagaMax, wymiarX, wymiarY, iloscPodzialow, plikDoZapisu);
+		graf.wypiszNaWyjscie();
+
+
 	}
 }
